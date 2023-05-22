ee62ef2dfc7a32a0eb941f52f22a53730ac86001
DEVair Martins <devair.martins@gmail.com>
2023-05-22 17:11:43 -0300

    dadospessoais

--- src/components/Dados.jsx
+++ src/components/Dados.jsx
@@ -16,15 +16,27 @@ function Dados() {
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Dados Pessoais</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <h5>Nome: Devair Martins Pereira</h5>
+          <ul>
+            <li>Brasileiro</li>
+            <li>29 anos</li>
+            <li>Casado</li>
+            <li>Parada de Lucas, RJ</li>
+          </ul>
+          <h5>Um pouco mais sobre mim:</h5>
+          <ul>
+            <li>Sou católico e faço parte de um movimento chamado Escola de Evangelização Santo André</li>
+            <li>Desde novo sempre gostei muito de tecnologia e video games (God of War em 1°lugar)</li>
+            <li>Como um bom carioca, adoro futebol e torço para o Vasco da Gama (Por favor, sem piadas que o site vai cair kkkkkk)</li>
+            <li>Minha dignissíma também é uma progamadora: @IsabelaAlves</li>
+          </ul>
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
           <Button variant="primary" onClick={handleClose}>
-            Save Changes
+            Fechar
           </Button>
         </Modal.Footer>
       </Modal>
