965cf3cf6e65874c8ef8283b2831024c8a30dde0
DEVair Martins <devair.martins@gmail.com>
2023-06-06 15:01:07 -0300

    mensagem

--- src/components/Mensagem.jsx
+++ src/components/Mensagem.jsx
@@ -1,12 +1,40 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import emailjs from '@emailjs/browser'
 
 function Mensagem() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [email, setEmail] = useState('');
+  const [nomE, setNome] = useState('');
+  const [mensagem, setMensagem] = useState('')
+  const [pagina, setPagina] = useState('')
+
+  function enviarEmail(e){
+    e.preventDefault();
+
+    const templateParams = {
+      from_email: email,
+      from_nome: nomE,
+      from_mensagem: mensagem,
+      from_pagina: pagina
+    }
+
+    if (email != '' || nomE != '' || mensagem != '' || pagina != ''){
+      emailjs.send("service_0s8pl2q", "template_bpik06m", templateParams, "sJ2esEo7IHslJLodL")
+      .then((response)=>{
+        alert(`Obrigado por deixar sua mensagem, irei responder assim que possível!`, response.status, response.text)
+      }, (err) => {
+        alert(`Mensagem não enviada.`, err)
+      })
+    } else {
+      alert('Por favor, verifique os dados.')
+    }
+  }
 
   return (
     <>
@@ -23,12 +51,12 @@ function Mensagem() {
           Que tal deixar uma mensagem direta pra mim?<br />
           É só preencher os campos e ficar a vontade.
 
-          <form action="">
+          <form onSubmit={enviarEmail}>
             <div className="row g-3">
               <div class="row g-3">
                 <div class="col-sm-6">
                   <label for="firstName" class="form-label">Nome</label>
-                  <input type="text" class="form-control" id="firstName" />
+                  <input type="text" class="form-control" id="firstName" onChange={(e)=>setNome(e.target.value)}/>
                 </div>
 
                 <div class="col-sm-6">
@@ -41,7 +69,7 @@ function Mensagem() {
 
                 <div class="col-12">
                   <label for="email" class="form-label">Email</label>
-                  <input type="email" class="form-control" id="email" placeholder="seu@exemplo.com" />
+                  <input type="email" class="form-control" id="email" placeholder="seu@exemplo.com" onChange={(e)=>setEmail(e.target.value)} />
                   <div class="invalid-feedback">
                     Please enter a valid email address for shipping updates.
                   </div>
@@ -51,7 +79,7 @@ function Mensagem() {
                   <label for="username" class="form-label">Sua Página <span class="text-muted">(Linkedin | Instagram | GitHub)</span></label>
                   <div class="input-group has-validation">
                     <span class="input-group-text">@</span>
-                    <input type="text" class="form-control" id="username"required />
+                    <input type="text" class="form-control" id="username" required onChange={(e)=>setPagina(e.target.value)} />
                     {/* <div class="invalid-feedback">
                       Your username is required.
                     </div> */}
@@ -60,22 +88,17 @@ function Mensagem() {
 
                 <div class="col-12">
                   <label for="address" class="form-label">Mensagem:</label>
-                  <textarea class="form-control" cols="30" rows="5"></textarea>
-                  {/* <div class="invalid-feedback">
-                    Please enter your shipping address.
-                  </div> */}
+                  <input type="text" onChange={(e)=>setMensagem(e.target.value)}/>
+                  {/* <textarea class="form-control" cols="30" rows="5" onChange={(e)=>setMensagem(e.target.value)}></textarea> */}
                 </div>
-
               </div>
+              <button className='btn btn-primary' type='submit'>Enviar Mensagem</button>
             </div>
           </form>
 
 
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="primary" onClick={handleClose}>
-            Enviar
-          </Button>
           <Button variant="danger" onClick={handleClose}>
             Close
           </Button>
