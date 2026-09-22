5f736eebffbd45b4386c1abebbe4bd9d6e9d2558
Devair Martins <devair.martins@gmail.com>
2026-09-22 11:45:50 -0300

    correções de footer e myimg

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import inspector from 'vite-plugin-react-inspector'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    inspector({
+      editor: 'code',
+      openIn: 'editor',
+    }),
+  ],
   base: "/Portfolio/"
 })
